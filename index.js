60978e790fdac9a2af4b50e4bd94954711535801
Mateo <mateomazzucco4@gmail.com>
2021-07-28 17:56:58 -0300

    Subiendo ejercicios realizados.

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const { readFile } = require('fs');
+const port = 5000;
+
+app.get('/date', (req, res) => {
+    res.send(new Date().toLocaleDateString());
+})
+
+app.get('/text', (req, res) => {
+    readFile('./text.txt', 'utf-8', (err, txt) => {
+        if (err) {
+            return null;
+        }
+        res.send(txt);
+    })
+})
+
+app.get('/goodmorning', (req, res) => {
+    const nombre = req.body.nombre;
+    res.send(`Buenos dias ${nombre}`);
+})
+
+app.listen(port, () => {
+    console.log(`Corre en http://localhost:${port}`);
+})
